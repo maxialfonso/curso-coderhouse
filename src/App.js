b721fefd8cb4e886e54d0501afdb23811f7fef36
Maximiliano Alfonso <MAlfonso2@teco.com.ar>
2022-05-10 16:11:16 -0300

    commit inicial

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Primer APP en React Maxi Alfonso
         </p>
         <a
           className="App-link"
