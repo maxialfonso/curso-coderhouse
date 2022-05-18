6e9d73c31ca73fdeb64ee5b411960d082524a156
Maxi Alfonso <alfonso.maximiliano@gmail.com>
2022-05-18 11:36:53 -0300

    Se crea el componente ItemCount

---
+++ src/components/ItemCount/ItemCount.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import "./itemCount.css";
+import { IconButton, Grid, Button, Input } from '@mui/material';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
+
+const ItemCount = ({ stock, init }) => {
+
+    const [cantidad, setCantidad] = useState(init);
+
+    function restarCantidad() {
+        const resultado = cantidad - 1;
+        if (resultado < 0) {
+            return;
+        } else {
+            setCantidad(cantidad - 1);
+        }
+    }
+
+    function sumarCantidad() {
+        const resultado = cantidad + 1;
+        if (resultado > stock) {
+            return;
+        } else {
+            setCantidad(cantidad + 1);
+        }
+    }
+
+    return (
+        <Grid container>
+            <Grid className='flex' item xs={3}>
+                <IconButton onClick={restarCantidad} color="primary" component="span">
+                    <RemoveCircleOutlineIcon />
+                </IconButton>
+            </Grid>
+            <Grid className='flex' item xs={6}>
+                <Input
+                    value={cantidad}
+                    variant='standard'
+                    disableUnderline
+                    inputProps={{
+                        readOnly: true,
+                        style: { textAlign: 'center' }
+                    }}
+                />
+            </Grid>
+            <Grid className='flex' item xs={3}>
+                <IconButton onClick={sumarCantidad} color="primary" component="span">
+                    <AddCircleOutlineIcon />
+                </IconButton>
+            </Grid>
+            <Grid className='flex' item xs={12}>
+                <Button color="primary">
+                    Agregar al carrito
+                </Button>
+            </Grid>
+        </Grid>
+    )
+}
+
+export default ItemCount
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,8 +1,16 @@
+import { Box } from '@mui/material'
 import React from 'react'
+import ItemCount from '../ItemCount/ItemCount'
 
-const ItemListContainer = ({saludo}) => {
+
+const ItemListContainer = ({ saludo }) => {
   return (
-    <p>{saludo}</p>
+    <>
+      <p>{saludo}</p>
+      <Box sx={{ width: "170px" }}>
+        <ItemCount stock={10} init={5}/>
+      </Box>
+    </>
   )
 }
 
