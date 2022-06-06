ea81b825cfb9b3549590b91af18b24d00f0d6ace
Maxi Alfonso <alfonso.maximiliano@gmail.com>
2022-06-06 15:02:55 -0300

    Fecha de creación de Orden

--- src/services/fetch/fetchOrders.js
+++ src/services/fetch/fetchOrders.js
@@ -1,12 +1,15 @@
 //@ts-check
-import { collection, addDoc, getFirestore } from "firebase/firestore";
+import { collection, addDoc, getFirestore, serverTimestamp } from "firebase/firestore";
 import { discountStock } from "./fetchUpdateItem";
 
 export function newOrder(order) {
 
     return new Promise(async (resolve, reject) => {
-
+        
+        order.created_at = serverTimestamp();
+        
         const { items } = order;
+        
         const DB = getFirestore();
         const orders = collection(DB, "orders");
 
